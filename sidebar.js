a9d67d0ad71e4861fe4da37232239aa45f4fad71
rolealiu <rolealiu@rippletec.net>
2017-12-13 22:54:20 +0800

    修改教程页面显示效果

--- sidebar.js
+++ sidebar.js
@@ -12,45 +12,31 @@
     sidebarClass.prototype = {
         init: function() {
             var template = {
-                target: decodeURI(window.location.search).split('?target=')[1]
+                target: window.location.search.split('?target=')[1]
             }
-            editormd.emoji = {
-                path: "http://www.emoji-cheat-sheet.com/graphics/emojis/",
-                ext: ".png"
-            };
-
-            editormd.twemoji = {
-                path: "http://twemoji.maxcdn.com/72x72/",
-                ext: ".png"
-            };
             if (template.target) {
                 var http = new XMLHttpRequest();
                 http.open("GET", window.location.origin + window.location.pathname + template.target + '.md', true);
                 http.send(null);
                 http.onload = function() {
                     editormd.markdownToHTML("article-container-js", {
-                        markdown: http.responseText,
-                        sequenceDiagram: true,
-                        flowChart: true,
-                        emoji: true,
-                        tex: true,
-                        taskList : true
+                        markdown: http.responseText
                     })
                 }
             } else {
-                window.location.href = window.location.href + '?target=/md/index';
+                window.location.href = window.location.href + '?target=/html/index';
             }
 
         },
         paint: function(arg, elem) {
-            var href = decodeURI(window.location.href).replace(/\//g, '.');
+            var href = window.location.href.replace(/\//g, '.');
             var isCurrent = eval('/' + arg.sref + '/').test(href);
             if (isCurrent && arg.href) {
-                window.document.title = arg.title + '-eoLinker官方支持手册';
+                window.document.title = arg.title + '-eolinker-官方教程';
             }
             var template = {
                 html: '<li class="common-level-' + arg.level + (isCurrent ? (arg.href ? (' elem-active level' + arg.level) : (' level' + arg.level)) : ' hidden') + '" >' +
-                    '<p level="' + arg.level + '" class="' + (isCurrent ? 'ico_up' : 'ico_down') + '" onclick="eocourse.sidebarClass.click(this)"><a ' + (arg.href ? ('onclick="eocourse.sidebarClass.router(\'' + arg.href + '\')"') : '') + '>' + arg.title + (arg.childList ? '<span class="pull-right  ico"></span>' : '') + '</a></p>' +
+                '<p level="' + arg.level + '" class="' + (isCurrent ? 'ico_up' : 'ico_down') + '" onclick="eocourse.sidebarClass.click(this)"><a ' + (arg.href ? ('onclick="eocourse.sidebarClass.router(\'' + arg.href + '\')"') : '') + '>' + (arg.childList ? '<span class="pull-left  ico"></span>' : '<span class="pull-left unchild-span"></span>') + arg.title + '</a></p>' +
                     '</li>',
                 elem: null
             }
